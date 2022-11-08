8d1707c133c71822a7247cccf372d5a03074c0c9
Blake <blakepetersont@gmail.com>
2022-11-08 14:32:16 -0700

    added more code to logic.js and added the questions el to index.html

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -9,4 +9,39 @@ var timerEl = document.getElementById('time');
 var choisesEl = document.getElementById('choices');
 var submitBtn = document.getElementById('submit');
 var startBtn = document.getElementById('start');
-var initialsEl = document 
+var initialsEl = document.getElementById('initials');
+var feedBackEl = document.getElementById('feedback');
+
+function beginQuiz() {
+    
+    var startEl = document.getElementById('start-screen');
+    startEl.setAttribute('class', 'hide');
+
+    questionsEl.removeAttribute('class');
+
+    timerId = setInterval(timer, 1000);
+
+    timerEl.textContent = time;
+
+    nextQuestion();
+}
+
+function nextQuestion() {
+    var currentQuestion = questions[currentQuestionIndex];
+    
+    var title = document.getElementById('question-title');
+    title.textContent = currentQuestion.title;
+
+    choicesEl.innerHTML = '';
+
+    for (var i = 0; i < currentQuestion.choices.length; i++) {
+        var choice = currentQuestion.choices[i];
+        var choiceNode = document.createElement('button');
+        choiceNode.setAttribute('class', 'choice');
+        choiceNode.setAttribute('value', choice);
+
+        choiceNode.textContent = i + 1 + '. ' + choice;
+
+        choicesEl.appendChild(choiceNode);
+    }
+}
