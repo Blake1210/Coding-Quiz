76a2278183c01fdd93c78aea146575d11b00c42f
Blake <blakepetersont@gmail.com>
2022-11-29 15:57:30 -0700

    starting socres.js

---
+++ assets/js/scores.js
@@ -0,0 +1,14 @@
+function showHighscores() {
+    var highscores = JSON.parse(window.localStorage.getItem('highscores')) || [];
+
+    highscores.sort(function (a, b){
+        return b.score - a.score;
+    });
+
+    for (var i = 0; i < highscores.length; i += 1) {
+        var liTag = document.createElement('li');
+        liTag.textContent = highscores[i].initials + ' - ' + highscores[i].score;
+
+        var olEl = document.getElementById('highscores');
+    }
+}
