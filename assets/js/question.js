725aaeed9716f71c5435795274931a5716d5e4ff
Blake <blakepetersont@gmail.com>
2022-12-01 14:43:35 -0700

    added questions. now trying to figure out why answers aren't showing

---
+++ assets/js/question.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        title: "What is the name of the object that allows you to perform mathmatical tasks with the interpreter?",
+        choices: ["Math", "Count", "Number", "Solve", ],
+        answer: 'Math',
+    },
+    {
+        title: "What kind of statement is used to execute actions based on a trigger or condition?",
+        choices: ["Fired Event", "Regular Expression", "Conditional Statement", "Boolean Variable"],
+        answer: 'Conditional Statement',
+    },
+    {
+        title: "What is the type of loop that continues through a block of code as long as the specified condition remains TRUE?",
+        choices: ["While Loop", "Else Loop", "For Loop", "Conditional Loop"],
+        answer: 'While Loop',
+    },
+    {
+        title: "What is a JavaScript element that represents either TRUE or FALSE values?",
+        choices: ["RegExp", "Condition", "Event", "Boolean"],
+        answer: 'Boolean',
+    },
+    {
+        title: "This is what you call the guide that defines coding conventions for all projects.",
+        choices: ["Main textbook", "Developer's reference", "Coding dictionary", "Style guide"],
+        answer: 'Style guide'
+    }
+];
